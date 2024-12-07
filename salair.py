974e4302cd5b9a65761a2da08fc8b88427c6c87a
meriemmoukrim <meriemmoukrim76@gmail.com>
2024-12-07 19:28:53 +0100

    Update salair.py

--- salair.py
+++ salair.py
@@ -1,7 +1,7 @@
-print("ce programme permet de composée le salaire")
-salair_base=float(input("donner le salair de base : "))
-HS=float(input("donner heure sut  : "))
-anciente=float(input("donner l'anciente: "))
+print("ce programme permet de compsée le salaire")
+Salair_Base=float(input("donner le salair de base : "))
+HS=float(input("donner le heure sut : "))
+anciente=int(input("donner l'anciente : "))
 if 0 <  anciente < 2 :
     taux_anciente = 0
 elif 2 <= anciente < 5 :
@@ -14,39 +14,39 @@ elif 15 <= anciente < 20 :
     taux_anciente = 20
 else :
     taux_anciente = 25
-prime_anciente = salair_base * taux_anciente / 100
-print(f"le prime anciente est : {prime_anciente}")
-prime_HS = 80 * HS
-print(f"le prime HS est : {prime_HS}")
-SB = salair_base + prime_HS + prime_anciente
-print(f"le salaire brut est : {SB} ")
-mutuelle = (3 * SB) / 100
-print("la mutuelle est :", mutuelle)
-retraite = (6 *  SB) / 100
-print("la retraite est :", retraite )
-if 0 < SB <= 2500 :
-    taux = 0
-    deduction = 0
-elif 2500 < SB <= 4166 :
-    taux = 10
-    deduction = 250
-elif 4166 < SB <= 5000 :
-    taux = 20
-    deduction = 666.67
-elif 5000< SB <= 6666 :
-    taux = 30
-    deduction = 1166.67
-elif 6666 < SB <= 15000 :
-    taux = 34
-    deduction = 1433.33
-else :
-    taux = 38
-    deduction = 2033.33
-IR = SB * taux / 100  - deduction
-print(f"IR est :{IR}")
-salair_net = SB - mutuelle - retraite - IR
-print("le salair net est :", salair_net )
+Prime_anciente = (Salair_Base *  taux_anciente )/ 100
+print(f"le prime anciente est : {Prime_anciente}")
+Prime_HS = HS * 80
+print(f"le prime heure sut est : {Prime_HS}")
 
+SB = Salair_Base + Prime_HS  + Prime_anciente
+print(f"le salair brut est : {SB}")
+
+Mutuelle = (3 * SB ) / 100
+print(f"la mutuelle est : {Mutuelle} ")
+
+Retrait = (6 * SB ) / 100
+print(f"la retraite  est : {Retrait} ")
+
+if 0 < SB <= 2500:
+       Deduction = Taux = 0
+elif 2500 < SB <= 4166 :
+       Deduction , Taux = 250 , 10
+elif 4166 < SB <= 5000 :
+       Deduction , Taux = 666.67 , 20
+elif 5000 < SB <= 6666 :
+       Deduction , Taux = 1166.67 , 30
+elif 6666 < SB <= 15000 :
+       Deduction , Taux = 1433.33 , 34
+else :
+       Deduction, Taux = 2033.33, 38
+IR = (SB * Taux / 100 )- Deduction
+print(f"le IR est : {IR}")
+Salaire_net = SB - Mutuelle - Retrait - IR
+print(f"le salaire net est : {Salaire_net}")
+
+
+  
 
 
 
